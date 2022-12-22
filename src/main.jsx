6bb8e568c009d077cfab89d8ca826200a51673e9
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-12-22 12:41:33 +0000

    Create sidebar as root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
