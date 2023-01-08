a159306909417e856ce736cecd8c0440034f6267
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2023-01-08 18:16:49 +0000

    [WIP] Sent Messages Page

--- src/pages/main/SentMessages.jsx
+++ src/pages/main/SentMessages.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function SentMesssages() {
     return (
         <div>
-            <h1>Sent Message</h1>
+            <h1>Sent Messages</h1>
         </div>
     ); 
 }
